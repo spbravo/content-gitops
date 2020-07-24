a66dca8e1c9ce792ca7fbeafd46e06b0fc12e156
spbravo <spbravo@gmail.com>
2020-07-24 10:52:38 +0200

    Update app.py
    Intentionally misstype Universe

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! and the rest of the Universe"
+    return "Hello World! and the rest of the Unierse"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
