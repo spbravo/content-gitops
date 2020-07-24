33d4c025cd95c62c1e544d3594edda2340ee1874
spbravo <spbravo@gmail.com>
2020-07-24 13:52:12 +0200

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!. Modifico codigo. El docker hub ahora es privado"
+    return "Hello World!. Modifico version a la 5. El docker hub ahora es privado"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
