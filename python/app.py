f90d49cca7801e044ac4928807c781630ac62dae
spbravo <spbravo@gmail.com>
2020-07-24 13:43:26 +0200

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!. Esto deberia funcionar igual. El respositorio sigue siendo publico"
+    return "Hello World!. Modifico codigo. El respositorio sigue siendo publico"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
